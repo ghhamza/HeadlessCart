884e28e4f0348b0156c7ab03c3c58a9c47fd6bec
GHANDOURI Hamza <hamza.ghandouri@ext.europarl.europa.eu>
2023-07-25 01:15:48 +0200

    first structure

---
+++ backend/alembic/env.py
@@ -0,0 +1,64 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+
+from alembic import context
+
+# ---------------- added code here -------------------------#
+import os
+import sys
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+sys.path.append(BASE_DIR)
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+
+fileConfig(config.config_file_name)
+
+from database import models
+
+target_metadata = models.Base.metadata
+
+
+# ------------------------------------------------------------#
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
